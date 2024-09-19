e7b9c1e22abe3cd87a3fb1c5225768934302e93f
eid129121 <116874095+eid129121@users.noreply.github.com>
2024-09-19 20:04:09 +0900

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,1 +1,1 @@
-return {"rayco_yt","eim_a12aa","seep0707","zorara666","ContentDeletedBANifn","sagrsdhfdh","apple8072","super_cax4485","ROOM_EIEI"}
+return {"rayco_yt","eim_a12aa","seep0707","zorara666","ContentDeletedBANifn","sagrsdhfdh","apple8072","super_cax4485","ROOM_EIEI","sypt12345"}
